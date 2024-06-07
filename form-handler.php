24b5869fcd828825ac37f940d250d37226c3b32e
RishikeshB <rishikeshb19307@cse.ssn.edu.in>
2024-06-07 21:23:53 +0530

    fix:registration changes

--- form-handler.php
+++ form-handler.php
@@ -5,14 +5,14 @@
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
-    $email_from = 'info@yourwebsite.com';
+    $email_from = 'rishividyut2000@gmail.com';
     $email_subject = 'New Form Submission'
     $email_body = "User Name: $name.\n".
                     "User Email: $visitor_email.\n".
                     "Subject: $subject.\n".
                      "User Message: $message .\n";   
 
-$to = 'papunaberdzeni@gmail.com';
+$to = 'rishikeshb19307@cse.ssn.edu.in';
 $headers = "From: $email_from \r\n";
 $headers .="Reply-To: $visitor_email \r\n";
 
--- script.js
+++ script.js
@@ -5,3 +5,39 @@ function showMenu(){
 function hideMenu(){
     navLinks.style.top = '-800px'
 }
+
+var isValid=true;
+var name = document.getElementById('name');
+var email = document.getElementById('email');
+var subject = document.getElementById('subject');
+var message = document.getElementById('message');
+var errName = document.getElementById('nameError');
+var errEmail = document.getElementById('emailError');
+var errSubject = document.getElementById('subjectError');
+var errMsg = document.getElementById('messageError');
+var formElement = document.querySelector('form');
+formElement.addEventListener('submit',event=>{
+  console.log(event);
+  isValid = true;
+  if(name.value.length == 0) {
+    errName.style.display='block';
+    isValid = false;
+  }
+  if(email.value.length == 0) {
+    errEmail.style.display='block';
+    isValid = false
+  }
+  if(subject.value.length == 0) {
+    errSubject.style.display='block';
+    isValid = false
+  }
+  if(message.value.length == 0) {
+    errMsg.style.display='block';
+    isValid = false;
+  }
+  if(!isValid) {
+    event.preventDefault();
+  }
+})
+
+
